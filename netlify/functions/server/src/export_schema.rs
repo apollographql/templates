00ac6ea8d7588cbd6953677678b03f5acf3ef0f8
Dylan Anthony <dbanty@users.noreply.github.com>
2023-10-19 15:09:53 -0600

    style: reformat

--- netlify/functions/server/src/export_schema.rs
+++ netlify/functions/server/src/export_schema.rs
@@ -1,8 +1,10 @@
 use async_graphql::SDLExportOptions;
-
 use schema::SCHEMA;
 mod schema;
 
 fn main() {
-    print!("{}", SCHEMA.sdl_with_options(SDLExportOptions::new().federation()));
+    print!(
+        "{}",
+        SCHEMA.sdl_with_options(SDLExportOptions::new().federation())
+    );
 }
--- netlify/functions/server/src/main.rs
+++ netlify/functions/server/src/main.rs
@@ -1,16 +1,18 @@
+mod errors;
 mod handler;
 mod schema;
-mod errors;
 
-use lambda_http::{service_fn, run, Error};
+use handler::handle_request;
+use lambda_http::{run, service_fn, Error};
 use log::LevelFilter;
 use simple_logger::SimpleLogger;
 
-use handler::handle_request;
-
 #[tokio::main]
 async fn main() -> Result<(), Error> {
-    SimpleLogger::new().with_level(LevelFilter::Info).init().unwrap();
+    SimpleLogger::new()
+        .with_level(LevelFilter::Info)
+        .init()
+        .unwrap();
 
     let func = service_fn(handle_request);
     run(func).await?;
